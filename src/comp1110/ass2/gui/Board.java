92ac558eae066f68b827c81bf96ba255dd147858
Ranjth Kandasamy Senthil Kumar <u7004474@anu.edu.au>
2019-10-14 13:17:55 +1100

    Edited Board.java for task 11.

--- src/comp1110/ass2/gui/Board.java
+++ src/comp1110/ass2/gui/Board.java
@@ -31,7 +31,8 @@ import javafx.scene.paint.Color;
 import javafx.scene.paint.Paint;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
-
+import org.junit.Assert;
+import org.junit.Test;
 
 
 import java.util.*;
@@ -88,6 +89,24 @@ public class  Board extends Application {
         --> If exists, repeat task 8.
     */
 
+    //---------------------------------Task 11---------------------------------------------------------------------------
+    //Author: Ranjth
+    public static String generateChallenege(){
+        Random rand = new Random();
+        String challenge = "";
+        String colors = "RBWG";
+        boolean containsSolution = false;
+
+        while (!containsSolution) {
+            for (int i = 0; i < 9; i++)
+                challenge += colors.charAt(rand.nextInt(colors.length()));
+
+            if (FocusGame.getSolution(challenge) != null) {
+                containsSolution = true;
+            }
+        }
+        return challenge;
+    }
 
 
     // Author: Victor
